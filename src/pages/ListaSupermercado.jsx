448d8e80b14162025da5125e3e51a3186b1c90a5
Antonio Gabriel Orlandini <antoniororlandini@gmail.com>
2025-10-01 19:47:17 -0300

    01/10/2025 - Alterações da aula passada (dia 31/09/2025)

--- src/pages/ListaSupermercado.jsx
+++ src/pages/ListaSupermercado.jsx
@@ -4,6 +4,21 @@ export default function ListaSupermercado() {
   const [itens, setItens] = useState([])
   const [novoItem, setNovoItem] = useState("")
 
+  // Persistir os dados localmente 
+  useEffect(() => {
+    const itensSalvos = localStorage.getItem("ListaSupermercado")
+    if (itensSalvos) {
+        setItens(JSON.parse(itensSalvos))
+    }
+  },[])
+
+  useEffect(() => { 
+    if (itens.length > 0){
+        // Como listaSupermercado é um array em JS, o JSON.stringify
+        // é usado para converter em formato texto (json)
+        localStorage.setItem("ListaSupermercado", JSON.stringify(itens))
+    }
+  }, [itens])
 
   function adicionarItem() {
     if (!novoItem.trim()) return
@@ -26,65 +41,42 @@ export default function ListaSupermercado() {
     setItens(listaOrdenada)
   }
 
-  function removerItem(index) {
-        const listaAtualizada = itens.filter((item, i) => {
-            return i != index
-        })
+  function removerListaSupermercado(index) {
+    // Cria uma nova lista sem a tarefa que está no índice recebido
+    const listaAtualizada = itens.filter((tarefa, i) =>{
+        // Mantém todas as tarefas cujo índice é diferente do index
+        return i != index
+    })
 
-        setItens(listaAtualizada)
-    }
+    // Atualiza o estado com a nova lista
+    setItens(listaAtualizada)
+  }
 
   return (
     <div className="container mt-5" style={{ maxWidth: "500px" }}>
       <h2 className="text-center mb-4">Lista de Supermercado</h2>
 
       <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Digite um item (ex: arroz, leite, pão...)"
-          value={novoItem}
-          onChange={(e) => setNovoItem(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && adicionarItem}
+        <input type="text" className="form-control" placeholder="Digite um item (ex: arroz, leite, pão...)" value={novoItem}
+          onChange={(e) => setNovoItem(e.target.value)} onKeyDown={(e) => e.key === "Enter" && adicionarItem()}
         />
-        <button className="btn btn-success" onClick={adicionarItem}>
-          Adicionar
-        </button>
-        <button className="btn btn-warning btn-sm" onClick={ordenarItens}>
-          Ordenar Itens
-        </button>
+        <button className="btn btn-success" onClick={adicionarItem}> Adicionar </button>
+        <button className="btn btn-warning btn-sm" onClick={ordenarItens}> Ordenar Itens </button>
       </div>
 
       <ul className="list-group">
         {itens.length === 0 && (
-          <li className="list-group-item text-center text-muted">
-            Nenhum item adicionado
-          </li>
+          <li className="list-group-item text-center text-muted"> Nenhum item adicionado </li>
         )}
         {itens.map((item, index) => (
-          <li
-            key={index}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
-            <span
-              style={{
-                textDecoration: item.comprado ? "line-through" : "none",
-                color: item.comprado ? "gray" : "black",
-              }}
-            >
-              {item.nome}
-            </span>
-            {!item.comprado && (
-              <button
-                className="btn btn-primary btn-sm"
-                onClick={() => marcarComoComprado(index)}
-              >
-                Marcar como Comprado
-              </button>
-            )}
-            <button className="btn btn-danger btn-sm" onClick={() => removerItem(index)}>
-              Remover Item
-            </button>
+          <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+            <span style={{ textDecoration: item.comprado ? "line-through" : "none", color: item.comprado ? "gray" : "black",}}> {item.nome} </span>
+            <div>
+              {!item.comprado && (
+                <button className="btn btn-primary btn-sm me-2" onClick={() => marcarComoComprado(index)}> Marcar como Comprado </button>
+              )}
+                <button className="btn btn-danger btn-sm" onClick={() => removerListaSupermercado(index)}> Remover </button>
+            </div>
           </li>
         ))}
       </ul>
--- src/pages/tarefasSimples.jsx
+++ src/pages/tarefasSimples.jsx
@@ -1,76 +1,72 @@
 import { useState, useEffect } from "react"
 
 export default function TarefasSimples() {
-    
-    const [listaTarefas, setListaTarefas] = useState([])
+    const [listaDeTarefas, setListaDeTarefas] = useState([])
     const [novaTarefa, setNovaTarefa] = useState("")
 
     // Persistir os dados localmente 
     useEffect(() => {
-        // Como listaTarefas é um array em JS, o JSON.stringify é usado para converter em formato de texto (json)
-        localStorage.setItem("tarefasSimples", JSON.stringify(listaTarefas))
-        
-        /*
-        if (listaTarefas.length > 0) {
-            localStorage.setItem("tarefasSimples", JSON.stringify(listaTarefas))
+        const tarefasSalvas = localStorage.getItem("tarefasSimples")
+        if (tarefasSalvas) {
+            setListaDeTarefas(JSON.parse(tarefasSalvas))
         }
-        */
-    // Executar sempre que listaTarefas for alterado
-    }, [listaTarefas])
+    },[])
+    
+    // Executar sempre que listaDeTarefas for alterado
+    useEffect(() => { 
+        if (listaDeTarefas.length > 0){
+            // Como listaDeTarefas é um array em JS, o JSON.stringify
+            // é usado para converter em formato texto (json)
+            localStorage.setItem("tarefasSimples", JSON.stringify(listaDeTarefas))
+        }
+    }, [listaDeTarefas])
 
-    function removerTarefa(index) {
-        // Cria uma lista sem a posição do index, O Filter é colocado ai para verificar as posições da lista de tarefas, caso ele 
-        // chegue a posição que tem o mesmo número do index, ele pula a posição, fazendo que a nova lista não tenha o valor excluido
-        const listaAtualizada = listaTarefas.filter((tarefa, i) => {
-            return i != index
-        })
-
-        // Atualiza o estado com a nova lista
-        setListaTarefas(listaAtualizada)
-    }
-
-    function adicionarTarefa() {
-        
-        // Caso não tenha valor no input não será adicionado. O "trim" é para apagar "espaços" no inicio e no fim da string
-        if (novaTarefa.trim() == ""){
+    function adicionarTarefa(){
+        if (novaTarefa.trim() == ""){       // Caso não for digitado nada
             return
         }
-
-        // Se for digitado um texto valido
-        setListaTarefas([...listaTarefas, novaTarefa])
-        setNovaTarefa("")
+        // Se foi digitado um texto válido
+        // Cria um novo array com as tarefas antigas + a nova tarefa
+        setListaDeTarefas([...listaDeTarefas, novaTarefa])
+        setNovaTarefa("")           // Limpar o campo de digitação
     }
 
-    function ordenarTarefas() {
-        const listaOrdenada = [...listaTarefas].sort((a,b) => a.localeCompare(b))
-        setListaTarefas(listaOrdenada)
+    function removerTarefa(index) {
+        // Cria uma nova lista sem a tarefa que está no índice recebido
+        const listaAtualizada = listaDeTarefas.filter((tarefa, i) =>{
+            // Mantém todas as tarefas cujo índice é diferente do index
+            return i != index
+        })
+        // Atualiza o estado com a nova lista
+        setListaDeTarefas(listaAtualizada)
     }
     
-    return (
-        <div className="container mt-5" style={{ maxWidth: '500px' }}>
-            <h1 className="text-center mb-4">Tarefas Simples</h1>
+    function ordenarTarefas(){
+        const listaOrdenada = [...listaDeTarefas].sort((a,b) => a.localeCompare(b))
+        setListaDeTarefas(listaOrdenada)
+    }
 
+    return( 
+        <div className="container mt-5" style={{ maxWidth: "500px" }}>
+            <h1 className="text-center mb-4">Tarefas simples</h1>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Digite a nova tarefa..." value={novaTarefa} onChange={(event) => setNovaTarefa(event.target.value)}/>
-                <button className="btn btn-primary" onClick={adicionarTarefa}>Adicionar</button>
-                <button className="btn btn-warning btn-sm" onClick={ordenarTarefas}>Ordenar Tarefas</button>
+                <input type="text" className="form-control" placeholder="Digite a nova tarefa" value={novaTarefa}
+                    onChange={(e) => setNovaTarefa(e.target.value)}
+                />
+                <button className="btn btn-primary" onClick={adicionarTarefa}> Adicionar </button>
+                <button className="btn btn-warning btn-sm" onClick={ordenarTarefas}> Ordenar tarefas </button>
             </div>
 
             <ul className="list-group">
-
-                {/* No react você pode usar o && com "então", ou seja, SE (condicional) == True , então (&&) (retorno) */}
-                {listaTarefas.length == 0 && (
-                  <li className="list-group-item text-center text-muted">Nenhuma tarefas adicionada</li>  
+                {listaDeTarefas.length == 0 && (
+                    <li className="list-group-item text-center text-muted"> Nenhuma tarefa adicionada </li>
                 )}
-
-                {/* map - seria um for, porem entregando já dois valores. Exemplo: lista.map(a, b), o "a" é o valor do array, o "b" é a posição do valor no item */}
-                {listaTarefas.map((tarefa, index) => (
-                    <li className="list-group-item d-flex justify-content-between" key={index}>
+                {listaDeTarefas.map((tarefa, posicaoNoIndex) => (
+                    <li key={posicaoNoIndex} className="list-group-item d-flex justify-content-between">
                         {tarefa}
-                        <button className="btn btn-danger btn-sm" onClick={() => removerTarefa(index)}>Remover</button>
+                        <button className="btn btn-danger btn-sm" onClick={() => removerTarefa(posicaoNoIndex)}> Remover </button>
                     </li>
                 ))}
-
             </ul>
         </div>
     )
